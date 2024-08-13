e9317009494f849c91d1c943bf52ad078d5d5e31
Christopher-DeSantiago-Rheem <137524467+ChrisD-Rheem@users.noreply.github.com>
2024-08-13 10:27:58 -0500

    Update hp_cds.py
    Updated CDS test script

--- 688/hp_cds.py
+++ 688/hp_cds.py
@@ -26,7 +26,7 @@ TEST_CASE_VERSION = "01.00.00"
  
 #use the first def in case of prompt user needed
 #def tc_xxxx(bench_test, log_config, cmd_msg_que, response_que, econet_write_obj_que, econet_read_obj_que, kill_que, prompt_que):
-def tc_xxxx(bench_test, log_config, log_lock, response_que, econet_write_obj_que, econet_read_obj_que, kill_que):
+def hp_cds(bench_test, log_config, log_lock, response_que, econet_write_obj_que, econet_read_obj_que, kill_que):
     """
     Test Case Requirement:
     ACHPC-SR-211 - Water Overflow/Drain Sense
@@ -66,12 +66,10 @@ def tc_xxxx(bench_test, log_config, log_lock, response_que, econet_write_obj_que
  
     Error Injection: no
  
-    Test Group: xxxx
+    Test Group: CommercialHP
  
     System Config:
         Indoor Unit: xxxx Valid values: Variable Speed Air Handler, Modulating Furnace, 2-Stage Furnace, Constant Torque Air Handler
-        Outdoor Unit: xxxx Valid values: Gen 1 RP2036, RA17 2 Stage, RP17 3 Speed
-        Water Heater: xxxx Valid values: HPWH1, HPWH2, HPWH3
  
     Raises:
         ConfigError
@@ -103,59 +101,34 @@ def tc_xxxx(bench_test, log_config, log_lock, response_que, econet_write_obj_que
     try:
         #Make sure that the test case is setup to run the specific system according to the test case header
         sys_setup(tc_logger, response_que, econet_write_obj_que, econet_read_obj_que, system_config, file_name, True, bench_test)
-       
         #****************************************************************************************************************
-        #****************************************************************************************************************
-        #  xxxx Put code here...
-        #
-        # Example of a read object command for the EcoNet object SPT
-        # read_resp = read_obj(tc_logger, response_que, econet_read_obj_que, "SPT")
-        # current_temp = read_resp[1][0]
-        #
-        # Example of a read object with a compare command and a +/- 0.5 tolerance
-        # read_obj_compare(tc_logger, response_que, econet_read_obj_que, "SPT", current_temp, 0.5)
-        #
-        # Example of a write object command to set the EcoNet object COOLSETP to 70.0 degrees
-        # Write the value for the COOLSETP
-        # obj_value = 70.0
-        # tc_logger.log_entry(f'***** Setting COOLSETP to {obj_value:2.1f}')
-        # write_obj(tc_logger, response_que, econet_write_obj_que, "COOLSETP", obj_value)
-        # tc_logger.log_entry(f'***** COOLSETP set to {obj_value:2.1f}')
-        #
-        # Byte Stream Read
-        # Example of byte stream read, and access specific object within the bytestream. Here we are reading the
-        # bytestream HWELMODL, and accessing the objects mdprtnum and cfmdhuma.
-        # With read_obj returning a bytestream class, we have access to all the object within as
-        # instance variables (in lowercase).
-        # tc_logger.log_entry("***** Reading HWELMODL")
-        # obj_read = read_obj(tc_logger, response_que, econet_read_obj_que, "HWELMODL", NetworkAddresses.ECONET_FURNACE)
-        # hwelmodl_obj = obj_read[1]
-        # tc_logger.log_entry(f'***** HWELMODL.MDPRTNUM: {hwelmodl_obj.mdprtnum}')
-        # tc_logger.log_entry(f'***** HWELMODL.CFMDHUMA: {hwelmodl_obj.cfmdhuma}')
-        #
-        # For byte stream write, we have two ways of writing to a bytestream. First we have single write.
-        # Example of single-object write, no previous byte stream read is needed.
-        # Note that the last parameter in write_bytestream is the string of the object in lowercase
-        # tc_logger.log_entry(f'***** changing HWELMODL.mdprtnum to 1234567890123456')
-        # write_bytestream(tc_logger, response_que,  econet_read_obj_que, econet_write_obj_que, "HWELMODL",
-        #                   '1234567890123456', NetworkAddresses.ECONET_FURNACE, False, "mdprtnum")
-        #
-        # For the multi-object write to work, a bytestream read must be performed beforehand so you can have access to
-        # the bytestream class.Note instead of passing in a value as the object value to be written,
-        # we make changes directly to the class attributes, and pass in the actuall bytestream class (hwelmodl_obj)    
-        # tc_logger.log_entry(f'***** changing HWELMODL.mdprtnum to 1234567890123456')
-        # tc_logger.log_entry(f'***** changing HWELMODL.cfmdhuma to 20')
-        # hwelmodl_obj.mdprtnum = '1234567890123456'
-        # hwelmodl_obj.cfmdhuma  = 20        
-        # write_obj(tc_logger, response_que, econet_write_obj_que, "HWELMODL", hwelmodl_obj, NetworkAddresses.ECONET_FURNACE)
-        #
-        # Example of how to prompt the user for user input.  Note if using prompts, comment in all uses of prompt_que
-        # Prompt the user to verify that the outdoor ambient sensor is setup properly
-        # user_prompt = (f'Is the outdoor ambient sensor setup? (Y/N) ==>')
-        # if not prompt_user(tc_logger, prompt_que, user_prompt):
-        #     raise UserInputIndicatedError(f'System not setup properly', logging.CRITICAL)
-        #
-        #****************************************************************************************************************
+    
+        # Reset microcontroller and wait for varying resistance and to clear all forced objects
+        tc_logger.log_entry('***** Resetting microcontroller')
+        write_obj(tc_logger, response_que, econet_write_obj_que, 'RESETDEV', SystemValues.YES, NetworkAddresses.ECONET_XXXX)
+
+        # Step 1. Waiting for 60 seconds (20 seconds recommended time in test case but can take 60 seconds for power-up)
+        if not bench_test:
+            tc_logger.log_entry('***** Waiting 60 seconds for board to power back up and detect varying CDS resistance')
+            time.sleep(60)
+
+        # Step 2. Setting the value for CDS_TEMR to 0 ohms so that the resistance is low enough to trigger a water sensed condition. Wait at
+        # least 45 seconds to allow for water sensed condtition to be logged. LOW_FAIL_RESISTANCE is 0 ohms.
+        tc_logger.log_entry(f'***** Setting CDS resistance to {ControlValues.LOW_FAIL_RESISTANCE}')
+        write_obj(tc_logger, response_que, econet_write_obj_que, "CDS_TEMR", ControlValues.LOW_FAIL_RESISTANCE, NetworkAddresses.ECONET_XXXX)   
+
+        # Step 3. Verify water sensed fault XX is active due to the resistance being below the decision point of 150K ohms.
+        if not bench_test:
+            tc_logger.log_entry('***** Waiting 45 seconds for potential fault to set')
+            time.sleep(45)
+        tc_logger.log_entry('***** Verifying fault XXA is active')
+        alarm_01 = read_obj(tc_logger, response_que, econet_read_obj_que, 'ALARM_01', NetworkAddresses.ECONET_XXXX)[1]
+        if alarm_01[0:6] != "T0XX_A" and not bench_test: #Only want preform the raise this if alarm is not active and if we are not in a benchtest
+            raise ValueCompareError(f'Fault T0XX_A not active')
+
+        # Step 4. Reset microcontroller to clear all forced objects
+        tc_logger.log_entry('***** Resetting microcontroller')
+        write_obj(tc_logger, response_que, econet_write_obj_que, 'RESETDEV', SystemValues.YES, NetworkAddresses.ECONET_XXXX)
         #****************************************************************************************************************
        
         tc_logger.log_entry(f'{req_trace_csv_data}Pass')
@@ -166,34 +139,28 @@ def tc_xxxx(bench_test, log_config, log_lock, response_que, econet_write_obj_que
         tc_logger.log_entry(f'***** *** Fatal error in {TEST_CASE_NAME}: {config_err} ***', logging.CRITICAL)
         tc_logger.log_entry(f'{req_trace_csv_data} Fail', logging.CRITICAL)
         return_code = (TEST_CASE_NAME, ReturnCodes.INVALID_SYS_CONFIG[1], ReturnCodes.INVALID_SYS_CONFIG[2])
- 
     except ReadObjError as read_obj_err:
         tc_logger.log_entry(f'***** *** Fatal error in {TEST_CASE_NAME}: {read_obj_err} ***', logging.CRITICAL)
         tc_logger.log_entry(f'{req_trace_csv_data} Fail', logging.CRITICAL)
         return_code = (TEST_CASE_NAME, ReturnCodes.READ_OBJ_ERROR[1], ReturnCodes.READ_OBJ_ERROR[2])
- 
     except WriteObjError as write_obj_err:
         tc_logger.log_entry(f'***** *** Fatal error in {TEST_CASE_NAME}: {write_obj_err} ***', logging.CRITICAL)
         tc_logger.log_entry(f'{req_trace_csv_data} Fail', logging.CRITICAL)
         return_code = (TEST_CASE_NAME, ReturnCodes.WRITE_OBJ_ERROR[1], ReturnCodes.WRITE_OBJ_ERROR[2])
- 
     except ValueCompareError as value_err:
         tc_logger.log_entry(f'***** *** Fatal error in {TEST_CASE_NAME}: {value_err} ***', logging.CRITICAL)
         tc_logger.log_entry(f'{req_trace_csv_data} Fail', logging.CRITICAL)
         return_code = (TEST_CASE_NAME, ReturnCodes.VALUE_COMPARE_ERROR[1], ReturnCodes.VALUE_COMPARE_ERROR[2])
-       
     except TerminateError as terminate_err:
         tc_logger.log_entry(f'***** *** Fatal error in {TEST_CASE_NAME}: {terminate_err} ***', logging.CRITICAL)
         tc_logger.log_entry(f'{req_trace_csv_data} Fail', logging.CRITICAL)
         return_code = (TEST_CASE_NAME, ReturnCodes.TERMINATE_ERROR[1], ReturnCodes.TERMINATE_ERROR[2])
- 
+
     finally:
-        #Ends the read/write process, shut system off(if necessary), and satisfies locktimr
+        #Ends the read/write process, shut system off(if necessary), and OATisfies locktimr
         end_process(tc_logger, bench_test, kill_que, response_que, econet_read_obj_que, econet_write_obj_que, turn_system_off=True)
         response_que.put(return_code)  
         # prompt_que.put(SystemFlags.PROMPTS_FINISHED)
-   
- 
 # Run the main() function to setup all of the processes and queues
 def main(bench_test = False):
     '''
@@ -204,40 +171,36 @@ def main(bench_test = False):
             econet_write_obj_que: Used for writing objects
             econet_read_obj_que: Used for reading objects
             kill_que: Used for killing the econet_read_write_process
- 
         Setup a log_lock for the logging queue
         Start all of the processes going
             Logging process: Used for logging all of the messages to the console and to a file
             bus_read_write_process: Used for writing to, and reading from the EcoNet bus which opens up the serial port
             test_case_process: Used to run the test case in
- 
+               
     Args:
         bench_test: True or False variable for bench testing only.
-
     Returns:
         None
+ 
     Objects used:
-        None 
+        None
     Raises:
         None
     '''
     #log_lock = Lock()
     log_que = Queue()
     log_configs = LoggingConfigs(log_que)
- 
     # Log the initial message for the test case to start
     main_logger = TestCaseLogger(log_configs.main_logging_config, TEST_CASE_NAME,
                                 "***** *** Starting " + TEST_CASE_NAME + " Version " + TEST_CASE_VERSION + " ***")
- 
     # Create an event to stop the logger
     logger_stop_event = Event()
-   
     # Create the logging process
     log_process = Process(target=logging_process, name='logger_process',
                           args=(log_configs.logging_process_config, log_que, logger_stop_event))
     log_process.start()
     main_logger.log_entry(f'Started process: {log_process.name}')
- 
+
     # Create the following queues:
     # response_que - Que that holds the response from the EcoNet bus
     # econet_write_que - Que that holds the object to write.  It is a tuple with the object name and object value
@@ -255,7 +218,7 @@ def main(bench_test = False):
                                             econet_write_obj_que, econet_read_obj_que, kill_que))
         bus_read_write_proc.start()
         main_logger.log_entry(f'Started process: {bus_read_write_proc.name}')
- 
+
         # Check to see if the bus_read_write_proc is running.  The only failure it can have is if the comm. port can't
         # be opened.  We need a small delay to allow the process to try and open the port and make sure that
         # everything is good before continuing
@@ -269,17 +232,14 @@ def main(bench_test = False):
             logger_stop_event.set()
             log_process.join()
             sys.exit(ReturnCodes.COMM_PORT_ERROR[1])
-       
- 
-        # Create and start the test case process
-        test_case_proc = Process(target=tc_xxxx, name=TEST_CASE_NAME,
+
+            # Create and start the test case process
+        test_case_proc = Process(target=hp_cds, name=TEST_CASE_NAME,
                                  args=(bench_test, log_configs.test_case_logging_config, cmd_msg_que,
                                        response_queue, econet_write_obj_que, econet_read_obj_que, kill_que))
- 
         test_case_proc.start()
         main_logger.log_entry(f'Started process: {test_case_proc.name}')
         time.sleep(0.5)
- 
         # Create a user prompt queue.  When a process is created, the stdin is closed.  Therefore, you cannot prompt
         # the user for input.  The main process has the stdin (keyboard input) still open.  So, a queue is created
         # to pass messages and responses back and forth to facilitate getting user input
@@ -297,7 +257,6 @@ def main(bench_test = False):
         # We now wait for the processes to finish their work.
         bus_read_write_proc.join()
         test_case_proc.join()
- 
         # All processes are done, so the logger can now stop.
         main_logger.log_entry("Telling logger to stop ...\n\n")
         logger_stop_event.set()
@@ -323,6 +282,7 @@ if __name__ == '__main__':
         if sys.argv[1] == "True":
             main(True)
         elif sys.argv[1] == "False":
+
             main(False)
         else:
             bad_format = True
@@ -332,4 +292,4 @@ if __name__ == '__main__':
     if bad_format:
         print("Incorrect number of command line arguments or incorrect values")
         print("Format:  module_name.py True|False")
-        print("Example:>python " + TEST_CASE_NAME + ".py True")         
+        print("Example:>python " + TEST_CASE_NAME + ".py True")  
